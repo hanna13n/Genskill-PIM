79482ceb962ed502270e28772c3c62a97f445b9e
hanna13n <hanna.nechikkadan@gmail.com>
2021-07-25 07:03:59 +0530

    edit and delete of notes done

--- backend/pim/notes.py
+++ backend/pim/notes.py
@@ -63,7 +63,7 @@ def notedetail(nid):
         "select t.tagname from hashtags t, tags_notes tn where t.id=tn.tag and tn.note=%s",(nid,)
         )
     tags=(x[0] for x in cursor.fetchall())
-    
+    tags=list(tags)
     if not note:
         if(request.accept_mimetypes.best == "application/json"):
             return jsonify({"error": f"No note with id {nid}"})
@@ -87,7 +87,7 @@ def notedetail(nid):
             tags=tags
         )
         return jsonify(ret)
-    else:
+    else: 
         return render_template("notes/notedetail.html", nid=nid, detail=detail, prev=prev, nxt=nxt, title=title, created_on=created_on, tags=tags)
 
 
@@ -103,7 +103,64 @@ def editnote(nid):
         "select t.tagname from hashtags t, tags_notes tn where t.id=tn.tag and tn.note=%s",(nid,)
         )
     tags=(x[0] for x in cursor.fetchall())
+    tags=list(tags)
+    cursor.execute(
+        "select tagname from hashtags"
+    )
+    
+    alltags=(x[0] for x in cursor.fetchall())
+    alltags=list(alltags)
 
+    if not note:
+        if(request.accept_mimetypes.best == "application/json"):
+            return jsonify({"error": f"No note with id {nid}"})
+        else:
+            return render_template("notes/notedetail.html"), 404
+
+    if request.method=="GET":
+        title, created_on, detail = note
+        return render_template("notes/editnote.html",
+        nid=nid,
+        detail=detail,
+        created_on=created_on,
+        title=title,
+        tags=tags,
+        alltags=alltags)
+    
+    elif request.method=="POST":
+        title=request.form.get("title")
+        detail=request.form.get("detail")
+    
+        for t in alltags:
+            if request.form.get(f"{t}"):
+                if t not in tags:
+                    cursor.execute(
+                        "insert into tags_notes(note,tag)values (%s,(select id from hashtags where tagname=%s))",(nid,t,)
+                    )
+            else:
+                if t in tags:
+                    cursor.execute(
+                        "delete from tags_notes where note=%s and tag=(select id from hashtags where tagname=%s)",(nid,t,)
+                    )
+        cursor.execute(
+            "update notes set title=%s, detail=%s where id=%s",(title,detail,nid)
+        )
+        conn.commit()
+        return redirect(url_for("notes.notedetail",nid=nid),302)
+
+@bp.route("/deletenote/<nid>")
+def deletenote(nid):
+    conn=db.get_db()
+    cursor=conn.cursor()
+    cursor.execute(
+        "delete from tags_notes where note=%s", (nid,)
+    )
+    cursor.execute(
+        "delete from notes where id=%s",(nid,)
+    )
+    conn.commit()
+    return redirect(url_for("notes.allnotes"),302)  
+    
 
 
 @bp.route("/newtag",methods=["GET","POST"])
