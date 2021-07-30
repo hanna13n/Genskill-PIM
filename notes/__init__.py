495bbc3c94ce715f9b702ae4ac851ca97198d1a5
hanna13n <hanna.nechikkadan@gmail.com>
2021-07-30 22:36:32 +0530

    :p

--- notes/__init__.py
+++ notes/__init__.py
@@ -6,9 +6,7 @@ from flask import jsonify
 def create_app():
     app = Flask("notes",static_folder="./frontend/build")
     CORS(app)
-    app.config.from_mapping(
-        DATABASE_URL="postgres://kstmushzcdgcpt:6bb2555f8a584aeb831597b814143bc49a07773f6b4f1a40913966f7148686ff@ec2-34-195-143-54.compute-1.amazonaws.com:5432/dg82d6od9h4fk"
-    )
+   
 
     from . import db
     db.init_app(app)
